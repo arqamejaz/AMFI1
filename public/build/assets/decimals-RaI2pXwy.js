135fe7f9b8ffbad735e6204dcb57912c642d19d6
M Arqam Ejaz <arqam.ejaz22@gmail.com>
2024-11-02 19:54:45 +0500

    before opt

---
+++ public/build/assets/decimals-RaI2pXwy.js
@@ -0,0 +1,1 @@
+import{cx as a}from"./app-BQfzREQL.js";import{d as e}from"./decimals-0pJ7t3BY.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
